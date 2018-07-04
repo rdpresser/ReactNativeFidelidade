af9340c0507b90f48e5c0a2075f97ff923350909
Rodrigo Presser <rodrigo.presser@gmail.com>
2018-07-04 14:31:03 -0300

    correções

--- App.js
+++ App.js
@@ -5,19 +5,12 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const geraNumeroAleatorio = () => {
+  const numeroAleatorio = Math.floor(Math.random() * 10);
+  alert(numeroAleatorio);
+};
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -27,12 +20,11 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button
+          title="Gerar um número randômico"
+          onPress={geraNumeroAleatorio}
+
+        />
       </View>
     );
   }
