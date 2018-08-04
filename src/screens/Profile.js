2ffa30d4a906f49158279a48f6690cd3119e1227
Rodrigo Presser <rodrigopresser@gmail.com>
2018-08-04 18:40:53 -0300

    correções para tirar foto do usuário logado

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { RNCamera } from 'react-native-camera';
+//import { View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import { View, StyleSheet } from 'react-native';
-import { Card, Button, Text } from 'react-native-elements';
+import { Card, Button, Text, Avatar } from 'react-native-elements';
 import { onSignOut, getUserLoginName } from '../auth';
 
 export default class Profile extends React.Component {
@@ -10,7 +12,9 @@ export default class Profile extends React.Component {
 
     this.state = {
       userLoginName: '',
-      userInitials: ''
+      userInitials: '',
+      showAvatar: false,
+      photoUri: ''
     };
   }
 
@@ -28,22 +32,65 @@ export default class Profile extends React.Component {
     }
   }
 
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true, fixOrientation: true };
+      const data = await this.camera.takePictureAsync(options);
+
+      this.setState({ showAvatar: true });
+      this.setState({ photoUri: data.uri });
+      //alert(data.uri);
+    }
+  }; 
+
   render() {
     return (
-      <View style={{ paddingVertical: 20 }}>
-        <Card title={this.state.userLoginName}>
-          <View style={styles.mainCard}>
-            <Text style={{ color: 'white', fontSize: 28 }}>{this.state.userInitials}</Text>
-          </View>
-          <Button
-            backgroundColor='#03A9F4'
-            title='Deslogar'
-            onPress={async () => {
-              await onSignOut();
-              this.props.navigation.navigate('SignedOut');
+      <View style={styles.container}>
+          <Card title={this.state.userLoginName}>
+            <View style={styles.mainCard}>
+              {!this.state.showAvatar ? 
+                <Avatar 
+                  large  
+                  rounded 
+                  //title={this.state.userInitials} 
+                  source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg' }}
+                  activeOpacity={0.7} 
+                />
+              : 
+              <Avatar 
+                large 
+                rounded
+                source={{ uri: this.state.photoUri }}
+                activeOpacity={0.7} 
+              />
+              }
+            </View>
+            <Button
+              backgroundColor='#03A9F4'
+              title='Deslogar'
+              onPress={async () => {
+                await onSignOut();
+                this.props.navigation.navigate('SignedOut');
+              }
             }
-          }
+            />
+          </Card>
+          <RNCamera
+                ref={camera => { this.camera = camera; }}
+                style={styles.preview}
+                type={RNCamera.Constants.Type.front}
+                autoFocus={RNCamera.Constants.AutoFocus.off}
+                flashMode={RNCamera.Constants.FlashMode.off}
+                permissionDialogTitle={'Permissão para usar a camera'}
+                permissionDialogMessage={'We need your permission to use your camera phone'}
           />
+          <Card>
+            <Button
+              buttonStyle={{ marginTop: 5 }}
+              backgroundColor='#03A9F4'
+              title='Tirar Foto'
+              onPress={this.takePicture.bind(this)}
+            />
         </Card>
       </View>
       );
@@ -52,13 +99,27 @@ export default class Profile extends React.Component {
 
 const styles = StyleSheet.create({
   mainCard: {
-    backgroundColor: '#bcbec1',
-    alignItems: 'center',
+    alignItems: 'center', 
     justifyContent: 'center',
-    width: 80,
-    height: 80,
-    borderRadius: 40,
     alignSelf: 'center',
-    marginBottom: 20
+    marginBottom: 10
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#ecf0f1',
   }
 });
--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Config from 'react-native-config';
 import qs from 'query-string';
-import { RNCamera } from 'react-native-camera';
-import { View, StyleSheet, Alert, TouchableOpacity, Text } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
 import { Card, Button, FormLabel, FormInput } from 'react-native-elements';
 import { onSignIn, getUserInfo } from '../auth';
 import { API } from '../Services/ServiceApi';
@@ -12,7 +11,7 @@ export default class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
-    //valores default para efeitos de teste
+    //valores default para facilitar os testes de login
     this.state = {
       username: '62564463215',
       password: '6256'
@@ -30,38 +29,18 @@ export default class SignIn extends React.Component {
         password: password
       });
     }
-  }
-
-  takePicture = async () => {
-    if (this.camera) {
-      const options = { quality: 0.5, base64: true };
-      const data = await this.camera.takePictureAsync(options);
-      alert(data.uri);
-    }
-  };  
+  } 
 
   render() {
     return (
-
       <View style={styles.container}>
-        <RNCamera
-          ref={camera => { this.camera = camera; }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          autoFocus={RNCamera.Constants.AutoFocus.on}
-          flashMode={RNCamera.Constants.FlashMode.off}
-          permissionDialogTitle={'Permission to use camera'}
-          permissionDialogMessage={'We need your permission to use your camera phone'}
-        />
-
         <Card>
           <FormLabel>Documento</FormLabel>
           <FormInput 
-            placeholder='Informe documento...' 
+            placeholder='Informe o documento...' 
             defaultValue={this.state.username} 
             onChangeText={(username) => this.setState({ username })}
           />
-
           <FormLabel>Senha</FormLabel>
           <FormInput 
             secureTextEntry 
@@ -112,14 +91,6 @@ export default class SignIn extends React.Component {
               this.props.navigation.navigate('SignedIn');
             }}
           />
-          <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-            <TouchableOpacity
-                onPress={this.takePicture.bind(this)}
-                style={styles.capture}
-            >
-                <Text style={{ fontSize: 14 }}> SNAP </Text>
-            </TouchableOpacity>
-          </View>
         </Card>
       </View>
     );
@@ -141,19 +112,5 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'black',
     marginBottom: 10,
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20
   }
 });
