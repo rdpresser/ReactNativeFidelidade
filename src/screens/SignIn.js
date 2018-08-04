c8856fc9592db86df2dcdc036cbfcdbb0ecf2e7e
Rodrigo Presser <rodrigopresser@gmail.com>
2018-08-04 15:53:37 -0300

    inclusão de RNCamera e testes na tela de login tirando foto

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Config from 'react-native-config';
 import qs from 'query-string';
-import { View, StyleSheet, Alert } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import { View, StyleSheet, Alert, TouchableOpacity, Text } from 'react-native';
 import { Card, Button, FormLabel, FormInput } from 'react-native-elements';
 import { onSignIn, getUserInfo } from '../auth';
 import { API } from '../Services/ServiceApi';
 
-//const result = async () => await API.get();
-
 export default class SignIn extends React.Component {
-
+  
   constructor(props) {
     super(props);
 
@@ -33,10 +32,28 @@ export default class SignIn extends React.Component {
     }
   }
 
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      alert(data.uri);
+    }
+  };  
+
   render() {
     return (
 
-      <View style={styles.container}>    
+      <View style={styles.container}>
+        <RNCamera
+          ref={camera => { this.camera = camera; }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          autoFocus={RNCamera.Constants.AutoFocus.on}
+          flashMode={RNCamera.Constants.FlashMode.off}
+          permissionDialogTitle={'Permission to use camera'}
+          permissionDialogMessage={'We need your permission to use your camera phone'}
+        />
+
         <Card>
           <FormLabel>Documento</FormLabel>
           <FormInput 
@@ -95,6 +112,14 @@ export default class SignIn extends React.Component {
               this.props.navigation.navigate('SignedIn');
             }}
           />
+          <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+            <TouchableOpacity
+                onPress={this.takePicture.bind(this)}
+                style={styles.capture}
+            >
+                <Text style={{ fontSize: 14 }}> SNAP </Text>
+            </TouchableOpacity>
+          </View>
         </Card>
       </View>
     );
@@ -117,4 +142,18 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     marginBottom: 10,
   },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  }
 });
