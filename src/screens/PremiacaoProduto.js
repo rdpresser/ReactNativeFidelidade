c629ceb1c9023624c56e45f498b51764cb4ebcfe
Rodrigo Presser <rodrigopresser@gmail.com>
2018-08-05 10:30:33 -0300

    tela de premiação retornando GET de premiação

--- src/screens/PremiacaoProduto.js
+++ src/screens/PremiacaoProduto.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import Config from 'react-native-config';
+import { ScrollView, Text, View, Alert } from 'react-native';
 import { Card } from 'react-native-elements';
+import { API } from '../Services/ServiceApi';
 
 export default class PremiacaoProduto extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      saldo: 42
+      saldo: 0
     };
+  }
+
+  async componentDidMount() {
+    //const saldo = await getUserSaldo();
+    const url = Config.API_PREMIACAO;
+    console.log(url);
+
+    const resultRequest = await API.get(url);
+    if (resultRequest.status !== 200) {
+      Alert.alert(resultRequest.statusText);
+      return;
+    }
+    
+    ///////////
+    console.log(resultRequest.data);    
+
+    //this.setState({ saldo });
+    //console.log(this.state.saldo);
   }
 
   render() {
